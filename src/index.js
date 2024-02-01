7993dee0f76d3f1434311511eeabe35e96db7499
ChakrabortyK <chakrabortyk21offc@gmail.com>
2024-02-02 00:42:35 +0530

    Routing changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/movie-app-imdb'>
       <Provider store={store}>
         <App />
       </Provider>
